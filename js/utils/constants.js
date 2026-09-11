d02a1417db4a5aa082f19ac56d98cab44604beb6
HEX <144029578+HEXU13PL@users.noreply.github.com>
2026-09-11 12:25:34 +0200

    Update constants.js

--- js/utils/constants.js
+++ js/utils/constants.js
@@ -1,5 +1,5 @@
-//  endpoints
-export const API_BASE_URL = `https://frontend.cfx-services.net/api`;
+// endpoints
+export const API_BASE_URL = 'https://servers-frontend.fivem.net/api/servers/single';
 export const SERVERS_ICON_URL = 'https://servers-live.fivem.net/servers/icon';
 
 // links templates
@@ -8,10 +8,8 @@ export const DISCORD_PROFILE_URL = 'https://discord.com/users/%id%';
 
 // default headers for fetch requests
 export const DEFAULT_HEADERS = {
-	Accept: '*/*',
-	'Content-Type': 'application/json',
-	'User-Agent':
-		'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+	'Accept': 'application/json',
+	'Content-Type': 'application/json'
 };
 
 // Theme options
@@ -34,10 +32,7 @@ export const STORAGE_KEYS = {
 export const DEFAULT_REFRESH_TIME = 30;
 
 export const PROXIES = [
-	'',
-	'https://corsproxy.io/',
-	'https://cors-proxy.htmldriven.com/?url=',
-	'https://thingproxy.freeboard.io/fetch/',
+	'https://corsproxy.io/?',
 	'https://api.allorigins.win/raw?url=',
-	'https://test.cors.workers.dev/?',
+	''
 ];
