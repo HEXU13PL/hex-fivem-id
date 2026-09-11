11a5f14ede1598c5343c677b501124b6d4e8e584
HEX <144029578+HEXU13PL@users.noreply.github.com>
2026-09-11 14:01:56 +0000

    asd

--- js/fetch.js
+++ js/fetch.js
@@ -263,10 +263,10 @@ export const renderPlayers = (players, search = false) => {
 	span2.appendChild(document.createTextNode('Created by '));
 
 	const link = document.createElement('a');
-	link.href = '';
+	link.href = 'https://github.com/igorovh';
 	link.target = '_blank';
 	link.rel = 'noopener noreferrer';
-	link.textContent = '';
+	link.textContent = 'igorovh';
 
 	span2.appendChild(link);
 	span2.appendChild(document.createTextNode('.'));
