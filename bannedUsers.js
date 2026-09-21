7221433de79e2dee345efe85f73aa5bc37eb6134
HEX <144029578+HEXU13PL@users.noreply.github.com>
2026-09-21 00:10:27 +0000

    asdsad

--- bannedUsers.js
+++
@@ -1,6 +0,0 @@
-export const BANNED_USERS = new Set([
-    'USR-3V8CYGU',
-    'USR-ZYTD5JQ',
-]);
-  // 666
-  // pozdro
--- js/blockedSessions.js
+++
@@ -1,4 +0,0 @@
-export const BLOCKED_SESSIONS = new Set([
-    'USR-OYGKTMH',
-    'USR-YYYYYYY',
-]);
